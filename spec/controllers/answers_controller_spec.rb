4071a29ead42e9999290f284084e396dafe383a3
Alexey Fomichev <thomas.web20@gmail.com>
2015-12-12 17:39:44 +0300

    Answers controller and tests

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let (:answer) { FactoryGirl.create(:answer) }
+  let (:question) { FactoryGirl.create(:question) }
+
+  describe "GET #new" do
+    before { get :new, question_id: question }
+
+    it 'assigns new Answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new template' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context 'valid' do
+      it 'saves new answer links with question in DB' do
+        expect {
+          post :create, question_id: question, answer: FactoryGirl.attributes_for(:answer)
+        }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirect to show' do
+        post :create, question_id: question, answer: FactoryGirl.attributes_for(:answer)
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+
+    context 'invalid' do
+      it 'does not save new answer in DB' do
+        expect {
+          post :create, question_id: question, answer: FactoryGirl.attributes_for(:invalid_answer)
+        }.to_not change(Answer, :count)
+      end
+
+      it 'renders show template' do
+        post :create, question_id: question, answer: FactoryGirl.attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :answer do
-    
+    body "Answer's Body"
   end
 
+  factory :invalid_answer, class: 'Answer' do
+    body nil
+  end
 end
