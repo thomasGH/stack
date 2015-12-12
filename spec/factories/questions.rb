0d73ae23dbef377727161c0578691dc6553f957c
Alexey Fomichev <thomas.web20@gmail.com>
2015-12-12 16:25:32 +0300

    Questions controller and tests

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :question do
-    
+    title "My question"
+    body "Question's Body"
   end
 
+  factory :invalid_question, class: 'Question' do
+    title nil
+    body nil
+  end
 end
