f6ed8776de13e3e83f82ae1e89a58eaf1628924a
Alexey Fomichev <thomas.web20@gmail.com>
2015-12-14 16:48:08 +0300

    Add validation on question_id, option destroy ant tests it

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe Question, type: :model do
 	it { should validate_presence_of(:title) }
 	it { should validate_presence_of(:body) }
 	it { should validate_length_of(:title).is_at_most(200) }
-	it { should have_many(:answers) }
+	it { should have_many(:answers).dependent(:destroy) }
 end
