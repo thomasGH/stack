dbc6c2cb366c8d495b25ae2fc4cdcc80fa9acdf5
Alexey Fomichev <thomas.web20@gmail.com>
2015-12-21 21:58:18 +0300

    Work in the class

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
     var form = $('#edit_answer_' + answer_id);
     var title = $('#answer_' + answer_id);
 
+
     if ($(this).hasClass('cancel')) {
-      $(this).html('<p>Edit</p>');
+      $(this).html('<p>Edit answer</p>');
       $(this).removeClass('cancel');
     } else {
       $(this).html('<p>Cancel</p>');
