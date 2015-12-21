a54a4867ebd9c8e3ec8032d7a1c121590f604e63
Alexey Fomichev <thomas.web20@gmail.com>
2015-12-21 17:32:57 +0300

    Inline edit answer

---
+++ app/assets/javascripts/answers.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('a.edit_answer_link').click(function() {
+    var answer_id = $(this).data('answerId');
+    var form = $('#edit_answer_' + answer_id);
+    var title = $('#answer_' + answer_id);
+
+    if ($(this).hasClass('cancel')) {
+      $(this).html('<p>Edit</p>');
+      $(this).removeClass('cancel');
+    } else {
+      $(this).html('<p>Cancel</p>');
+      $(this).addClass('cancel');
+    }
+    form.toggle();
+    title.toggle();
+  })
+})
