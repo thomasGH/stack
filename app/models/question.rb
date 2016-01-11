a12350649e160dae07fdefe22b7d44bef116bb0a
Alexey Fomichev <thomas.web20@gmail.com>
2016-01-11 14:44:23 +0300

    Change model Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
+  belongs_to :best_answer, class_name: "Answer"
 
   validates :title, :body, :user_id, presence: true
   validates :title, uniqueness: true, length: { maximum: 200 }
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should belong_to :user }
   it { should have_many(:answers).dependent(:destroy) }
+  it { should belong_to(:best_answer).class_name("Answer") }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
