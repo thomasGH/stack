ccf9a24cb741d1648ba3344974b157ebd850950c
Alexey Fomichev <thomas.web20@gmail.com>
2016-01-21 17:27:23 +0300

    Move code to sign_in_oauth method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,18 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     #render json: request.env['omniauth.auth']
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
+    sign_in_oauth('Facebook')
   end
 
   def github
+    sign_in_oauth('Github')
+  end
+
+  private
+
+  def sign_in_oauth(kind)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-    end
+      set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+    end    
   end
 end
