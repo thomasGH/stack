ddf2c198a7579fb75e8b6ef4b227282129162b67
Alexey Fomichev <thomas.web20@gmail.com>
2016-01-26 17:45:33 +0300

    Implementation API for profile and questions

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,13 +1,7 @@
-class Api::V1::ProfilesController < ApplicationController
-  before_action :doorkeeper_authorize!
+class Api::V1::ProfilesController < Api::V1::BaseController
+  authorize_resource class: User
 
   def me
-    render json: current_user
-  end
-
-  protected
-
-  def curent_resource_owner
-
+    render json: current_resource_owner
   end
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,3 +1,4 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :body, :created_at, :updated_at
+  has_many :answers
 end
