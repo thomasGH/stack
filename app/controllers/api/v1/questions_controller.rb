9e3fd2c6b8e03a744bd2f7389b63198971308894
Alexey Fomichev <thomas.web20@gmail.com>
2016-01-28 13:25:25 +0300

    Try API method create

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -10,4 +10,17 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     @question = Question.find(params[:id])
     render json: @question
   end
+
+  # Don'w work :( ActionController::InvalidAuthenticityToken
+  #
+  # def create
+  #   @question = Question.new(params.require(:question).permit(:title, :body))
+  #   @question.user = current_resource_owner.id
+
+  #   if @question.save
+  #     render json, nothing: true, status: 200
+  #   else
+  #     render json: @question.errors.full_messages, status: :unprocessable_entity
+  #   end
+  # end
 end
