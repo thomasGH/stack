d0cb6d8e0e357bf3b7c8132272d10106f54d32ca
Alexey Fomichev <thomas.web20@gmail.com>
2016-02-04 13:10:24 +0300

    Use concern

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,10 +5,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
   belongs_to :best_answer, class_name: "Answer"
-  has_many :attachments, as: :attachable, dependent: :destroy
 
   validates :title, :body, :user_id, presence: true
   validates :title, uniqueness: true, length: { maximum: 200 }
-
-  accepts_nested_attributes_for :attachments
 end
