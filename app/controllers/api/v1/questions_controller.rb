b0a52f7caa2bc281c32dcbd36bea0f2a43f7a913
Alexey Fomichev <thomas.web20@gmail.com>
2016-02-05 15:33:44 +0300

    API for create questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -11,16 +11,23 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     render json: @question
   end
 
-  # Don'w work :( ActionController::InvalidAuthenticityToken
-  #
-  # def create
-  #   @question = Question.new(params.require(:question).permit(:title, :body))
-  #   @question.user = current_resource_owner.id
 
-  #   if @question.save
-  #     render json, nothing: true, status: 200
-  #   else
-  #     render json: @question.errors.full_messages, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+
+    @question = Question.new(question_params)
+
+    @question.user = current_resource_owner
+
+    if @question.save
+      render nothing: true, status: 200
+    else
+      render json: @question.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
