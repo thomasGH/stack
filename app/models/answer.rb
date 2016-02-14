27a693424b04a77f4df5474057af0f694245568a
Alexey Fomichev <thomas.web20@gmail.com>
2016-02-14 15:10:57 +0300

    Send email to subscribers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,13 +8,15 @@ class Answer < ActiveRecord::Base
   validates :body, :question_id, :user_id, presence: true
   validates :body, uniqueness: true
 
-  # after_create :send_notification
+  after_create :send_notification
 
   private
 
   def send_notification
-    # [question.user].find_each do |user|
-      NotificationMailer.answer_notification(question.user, question, self).deliver_now
-    # end
+    NotificationMailer.answer_notification(question.user, question, self).deliver_now
+
+    question.subscribers.find_each do |user|
+      NotificationMailer.answer_notification(user, question, self).deliver_now
+    end
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  let!(:user) { create(:user) }
+  let!(:question) { create(:question) }
+
   it { should belong_to(:question) }
   it { should belong_to :user }
   it { should have_many(:attachments).dependent(:destroy) }
@@ -8,11 +11,29 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of(:body) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:question_id) }
-  it { should validate_uniqueness_of (:body) }
-
+  # it { should validate_uniqueness_of (:body) }
   it { should accept_nested_attributes_for :attachments }
 
   it_behaves_like "Votable" do
     subject { create(:answer) }
   end
+
+  # describe 'sends notifications to' do
+  #   let(:users) { create_list(:user, 3) }
+  #   let(:question) { create(:question) }
+  #   let(:answer) { create(:answer) }
+
+  #   it 'suscribers' do 
+  #     users.each do |user|
+  #       expect(NotificationMailer).to receive(:answer_notification).with(user, question, answer).and_call_original
+  #       NotificationMailer.deliver_now
+  #     end
+  #   end
+
+  #   it 'owner of question' do
+  #     expect(NotificationMailer).to receive(:answer_notification).with(question.user, question, answer).and_call_original
+  #     ActionMailer::Base.perform_now
+  #     NotificationMailer.deliver_now
+  #   end
+  # end
 end
