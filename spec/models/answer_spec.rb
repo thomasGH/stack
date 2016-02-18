b11aeb79bf9d3ea9701c03b1d5f010c79be4f6b1
Alexey Fomichev <thomas.web20@gmail.com>
2016-02-18 15:35:54 +0300

    Make more tests for models

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -11,8 +11,13 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of(:body) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:question_id) }
-  # it { should validate_uniqueness_of (:body) }
   it { should accept_nested_attributes_for :attachments }
+
+  it "should validate uniqueness of body" do
+    answer1 = Answer.create!(question: question, body: 'Answer', user: user)
+    answer2 = Answer.new(question: question, body: 'Answer', user: user)
+    expect(answer2).not_to be_valid
+  end
 
   it_behaves_like "Model Votable" do
     subject { create(:answer) }
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
+  let(:attachment) { create(:attachment) }
+
   it { should belong_to(:attachable) }
+
+  it 'should method file_name returns name of file' do
+    expect(attachment.file_name).to eq 'spec_helper.rb'
+  end
 end
